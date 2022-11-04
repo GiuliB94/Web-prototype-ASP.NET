870290b216c7d582c1f4e309a4d838c58b0b8799
GiuliB94 <giuliananboggio@gmail.com>
2022-11-04 00:26:45 -0300

    primer commit

---
+++ effort_ver1/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace effort_ver1
+{
+    public class Cliente
+    {
+        public int IdUser { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+        public int IdEmpresa { get; set; }
+        public string email { get; set; }   
+        public string telefono { get; set; }
+
+    }
+}
---
+++ effort_ver1/Empresa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace effort_ver1
+{
+    public class Empresa
+    {
+        public int IdEmpresa { get; set; }
+        public int Cuit { get; set; }
+        public string RazonSocial {get; set; }
+        public string Calle {get; set; }
+        public int NumCalle { get; set; }
+        public int Cp { get; set; }
+        public string Localidad { get; set; }
+
+    }
+}
