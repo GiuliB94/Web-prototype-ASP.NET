db5130d6ce6980e438054c95d26ec27edfdcba08
Julieta <julieta.corbacho@gmail.com>
2022-11-04 01:42:45 -0300

    Agregado de clases Material y Producto

---
+++ effort_ver1/Material.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace effort_ver1
+{
+    public class Material
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+        public int costo { get; set; }
+
+    }
+}
