2d81dbc302dbc5db7b24daea474237bc8a050608
GiuliB94 <giuliananboggio@gmail.com>
2022-11-14 13:52:51 -0300

    cambios steticosh

--- effort_ver1/Master.Master.designer.cs
+++ effort_ver1/Master.Master.designer.cs
@@ -33,15 +33,6 @@ namespace effort_ver1
         protected global::System.Web.UI.WebControls.ContentPlaceHolder Stylesheets;
 
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
         /// LogLink control.
         /// </summary>
         /// <remarks>
