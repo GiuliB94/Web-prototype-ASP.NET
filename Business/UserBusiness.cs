d5cdf5f5cd4c10912da4e68137ab8921645f1c65
lucasanche <lucasg.sanchez94@gmail.com>
2022-12-01 01:04:48 -0300

    eliminado seguimiento de carpetas innecesarias que se agregaron al gitignore

---
+++ Business/UserBusiness.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Domain;
+
+namespace Business
+{
+    public class UserBusiness
+    {
+        public bool LogIn(User user)
+        {
+            AccessData accessData = new AccessData();
+            try
+            {
+                accessData.setQuery("SELECT Id, userPermission FROM Users where userEmail = @Email AND pwd = @pwd;");
+                accessData.SetParameter("@user", user.UserEmail);
+                accessData.SetParameter("@pwd", user.Password);
+
+                accessData.executeQuery();
+                while (accessData.Reader.Read())
+                {
+                    user.Id = (int)accessData.Reader["Id"];
+                    user.Permission = (UserPermission)accessData.Reader["userPermission"];
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return true;
+        }
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public enum UserPermission
+    {
+        ADMIN = 0,
+        CLIENT1,
+        CLIENT2,
+        CLIENT3
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserEmail { get; set; }
+        public string Password { get; set; }
+        public UserPermission Permission { get; set; }
+
+        public User( string userEmail, string password, int permission)
+        {
+            UserEmail = userEmail;
+            Password = password;
+            Permission = (UserPermission)permission;
+        }
+    }
+}
