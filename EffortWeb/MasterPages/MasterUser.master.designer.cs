78183969f7b2d9fbc9a8571aa514b326821b19fe
lucasanche <lucasg.sanchez94@gmail.com>
2022-12-13 22:56:10 -0300

    merge con rama Juli

--- EffortWeb/MasterPages/MasterUser.master.designer.cs
+++ EffortWeb/MasterPages/MasterUser.master.designer.cs
@@ -42,6 +42,15 @@ namespace EffortWeb.MasterPages
         protected global::System.Web.UI.HtmlControls.HtmlForm formMasterPageUser;
 
         /// <summary>
+        /// lblUserEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUserEmail;
+
+        /// <summary>
         /// LogoutButton control.
         /// </summary>
         /// <remarks>
